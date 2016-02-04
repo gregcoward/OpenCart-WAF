50928fb471228d09d0af16179073773507ba4d10
tstanley93 <t.stanley@f5.com>
2016-02-04 08:15:40 -0800

    Many updates to move the LB Rules back and then call for them in waf template

--- OpenCart-WAF/start.sh
+++ OpenCart-WAF/start.sh
@@ -34,11 +34,12 @@
 ## devicearr=7 #master address
 ## vipportarr=0 #port numbers of the BIG-IP VIP semicolon delimited (80;443;8080)
 ## protocolarr=0 #protocol for the VIP like http or https semicolon delimited
-## hostarr=0 #ip address of the application servers, or fqdn of applicaiton
-## hostarr=1 #region or location of the URL (westus, eastus)
+## hostarr=0 #ip address of the first application server
+## hostarr=1 #ip address of the second application server
+## hostarr=2 #region or location of the URL (westus, eastus)
 ## appportarr=0 #the port of the application (80;443;8080)
 ## asmarr=0 # linux or windows
-## asmarr=1 #blocking level, high medium low
+## asmarr=1 # security blocking level, high medium low
 ## asmarr=2 #fqdn for the application
 ## asmarr=3 #secure string of SSL certificate file path
 ## asmarr=4 #secure string of SSL key file path
@@ -94,10 +95,10 @@ fi
 
 
 ## Construct the blackbox.conf file using the arrays.
-row1='"1":["'${vipportarr[0]}'","'${protocolarr[0]}'",["'${hostarr[0]}':'${appportarr[0]}'"],"","","","","","'${asmarr[0]}'","'${asmarr[1]}'","yes","yes","yes","wanlan","'${asmarr[2]}'","yes","","","","",""]'
-row2='"2":["'${vipportarr[1]}'","'${protocolarr[1]}'",["'${hostarr[0]}':'${appportarr[1]}'"],"","","","","","'${asmarr[0]}'","'${asmarr[1]}'","yes","yes","yes","wanlan","'${asmarr[2]}'","yes","","","'${certpath}'","'${keypath}'","'${chainpath}'"]'
+row1='"1":["'${vipportarr[0]}'","'${protocolarr[0]}'",["'${hostarr[0]}':'${appportarr[0]}'","'${hostarr[1]}':'${appportarr[0]}'"],"","","","","","'${asmarr[0]}'","'${asmarr[1]}'","yes","yes","yes","wanlan","'${asmarr[2]}'","yes","","","","",""]'
+row2='"2":["'${vipportarr[1]}'","'${protocolarr[1]}'",["'${hostarr[0]}':'${appportarr[1]}'","'${hostarr[1]}':'${appportarr[1]}'"],"","","","","","'${asmarr[0]}'","'${asmarr[1]}'","yes","yes","yes","wanlan","'${asmarr[2]}'","yes","","","'${certpath}'","'${keypath}'","'${chainpath}'"]'
 
-deployment1='deployment_'${devicearr[5]}'.'${hostarr[1]}'.cloudapp.azure.com":{"traffic-group":"none","strict-updates":"disabled","variables":{},"tables":{"configuration__destination":{"column-names":["port","mode","backendmembers","monitoruser","monitorpass","monitoruri","monitorexpect","asmtemplate","asmapptype","asmlevel","l7ddos","ipintel","caching","tcpoptmode","fqdns","oneconnect","sslpkcs12","sslpassphrase","sslcert","sslkey","sslchain"],"rows":{'$row1','$row2'}}}}'
+deployment1='deployment_'${devicearr[5]}'.'${hostarr[2]}'.cloudapp.azure.com":{"traffic-group":"none","strict-updates":"disabled","variables":{},"tables":{"configuration__destination":{"column-names":["port","mode","backendmembers","monitoruser","monitorpass","monitoruri","monitorexpect","asmtemplate","asmapptype","asmlevel","l7ddos","ipintel","caching","tcpoptmode","fqdns","oneconnect","sslpkcs12","sslpassphrase","sslcert","sslkey","sslchain"],"rows":{'$row1','$row2'}}}}'
 
 jsonfile='{"loadbalance":{"is_master":"'${devicearr[0]}'","master_hostname":"'${devicearr[6]}'","master_address":"'${devicearr[7]}'","master_password":"'${devicearr[3]}'","device_hostname":"'${devicearr[1]}'","device_address":"'${devicearr[2]}'","device_password":"'${devicearr[3]}'"},"logging":{"saskey":"tAjn8Xuzelj9ps4HzRsHXqXznAIiHPFIzlSC08De2Zk=","saskeyname":"sharing-is-caring","eventhub":"event-horizon","eventhub_namespace":"event-horizon-ns","logginglevel":"Alert","loggingtemplate":"CEF","applianceid":"8A3ED335-F734-449F-A8FB-335B48FE3B50"},"bigip":{"application_name":"Azure Security F5 WAF","ntp_servers":"1.pool.ntp.org 2.pool.ntp.org","ssh_key_inject":"false","change_passwords":"false","license":{"basekey":"'${devicearr[4]}'"},"modules":{"auto_provision":"true","ltm":"nominal","afm":"none","asm":"nominal"},"redundancy":{"provision":"false"},"network":{"provision":"false"},"iappconfig":{"f5.rome_waf_rc":{"template_location":"http://cdn-prod-ore-f5.s3-website-us-west-2.amazonaws.com/product/blackbox/staging/azure/f5.rome_waf_rc.tmpl","deployments":{"'$deployment1'}}}}}'
 
